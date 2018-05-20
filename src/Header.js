1e14d859335bf31d77954db22898a40b7a0bfe0e
Ahamad-PC <ahamad.jedu@gmail.com>
2018-05-20 10:51:55 +0700

    4.3-component by Stateless Functional Components

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,27 @@
 import React, {Component} from 'react';
 
-class Header extends Component {
-
-    render() {
-        return (
-            <div >
-                <h1>
-                    This is Header {this.props.title}
-                </h1>
-            </div>
-        )
-    }
-
+const Header = (props) => {
+    return (
+        <div>
+            <h1>This is Header {props.title}</h1>
+        </div>
+    )
 }
-
 export default Header
+
+
+// class Header extends Component {
+
+//     render() {
+//         return (
+//             <div >
+//                 <h1>
+//                     This is Header {this.props.title}
+//                 </h1>
+//             </div>
+//         )
+//     }
+
+// }
+
+// export default Header
